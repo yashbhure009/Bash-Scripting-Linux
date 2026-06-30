d3cb1bbe254241a61012b7e94ed7418fc1068bf6
Yash Bhure <bhureyash444@gmail.com>
2026-06-30 23:22:14 +0530

    Create Bash scripts for basic scripting examples
    Added Bash scripts for demonstration of basic scripting and array usage.

---
+++ Bash.sh
@@ -0,0 +1,212 @@
+┌──(kali㉿kali)-[~/Desktop]
+└─$ mkdir BashScripting                         
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop]
+└─$ ls 
+BashScripting  firefox-esr.desktop  kali-burpsuite.desktop
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop]
+└─$ cd BashScripting
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ls
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ echo "Hey My name is Yash Bhure"                           
+Hey My name is Yash Bhure
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript   
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ls
+myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ chmod +x myscript.sh 
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh 
+This is My First Script
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ls -la
+total 12
+drwxrwxr-x 2 kali kali 4096 Jun 30 16:49 .
+drwxr-xr-x 3 kali kali 4096 Jun 30 16:46 ..
+-rwxrwxr-x 1 kali kali   43 Jun 30 16:49 myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ls -la
+total 12
+drwxrwxr-x 2 kali kali 4096 Jun 30 16:49 .
+drwxr-xr-x 3 kali kali 4096 Jun 30 16:46 ..
+-rwxrwxr-x 1 kali kali   43 Jun 30 16:49 myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh
+This is My First Script
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ chmod +x myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh       
+This is My First Script
+I am Learning Linux
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh   
+This is My First Script
+I am Learning Linux
+And These are my network settings
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh   
+This is My First Script
+I am Learning Linux
+And These are my network settings
+Yash Bhure
+Nice to see you Yash Bhure
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh   
+This is My First Script
+I am Learning Linux
+And These are my network settings
+Yash Bhure
+Nice to see you Yash Bhure
+Yash Bhure 20 New York
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh   
+This is My First Script
+I am Learning Linux
+And These are my network settings
+Yash Bhure
+Nice to see you Yash Bhure
+Yash Bhure 20 New York
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh   
+This is My First Script
+I am Learning Linux
+And These are my network settings
+Yash Bhure
+Nice to see you Yash Bhure
+Yash Bhure 20 New York
+Heckur is 22 years old lives in France and France is in Paris
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano myscript.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./myscript.sh   
+This is My First Script
+I am Learning Linux
+And These are my network settings
+Yash Bhure
+Nice to see you Yash Bhure
+Yash Bhure 20 New York
+Heckur is 22 years old lives in France and France is in Paris
+Enter your name
+Yash Bhure
+Your name is Yash Bhure 
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano array.sh   
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ chmod +x array.sh   
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./array.sh   
+cartruckbikebus
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano array.sh    
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./array.sh       
+car truck bike bus
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano array.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./array.sh   
+car truck bike bus
+car
+truck
+bike
+bus
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano array.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./array.sh   
+car truck bike bus
+car
+truck
+bike
+bus
+car truck bike bus
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano array.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./array.sh   
+car truck bike bus
+car
+truck
+bike
+bus
+car bike bus
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano array.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./array.sh   
+car truck bike bus
+car
+truck
+bike
+bus
+car bike bus
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ nano array.sh
+                                                                                                                                                           
+┌──(kali㉿kali)-[~/Desktop/BashScripting]
+└─$ ./array.sh   
+car truck bike bus
+car
+truck
+bike
+bus
+car bike bus
+car Truck8chakka bike bus
